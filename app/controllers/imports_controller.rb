0b86f2fcf3504ee3ffb1d6b093655aabdde68d14
Sören Link (Macbook) <linksoeren@googlemail.com>
2014-02-26 09:49:15 +0100

    .gitignore changes. Updated account view

--- app/controllers/imports_controller.rb
+++
@@ -1,5 +0,0 @@
-class ImportsController < ApplicationController
-  def index
-    Import.import
-  end
-end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -7,14 +7,12 @@ class PowersController < ApplicationController
   def update
     @power = Power.find params[:id]
     if current_user == @power.character.user
-      puts params[:power].to_yaml
       case params[:power][:action]
       when "increase_usage"
         @power.increase_usage
       when "decrease_usage"
         @power.decrease_usage
       end
-      puts @power.to_yaml
       if @power.save
         @character = @power.character
         render 'update.json.jbuilder'

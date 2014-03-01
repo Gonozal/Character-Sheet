e5b0d529c115c18015e75b1d1778e3df9dd94095
Sören Link (Macbook) <linksoeren@googlemail.com>
2014-03-01 20:47:43 +0100

    Temporary fix to clean up failed char imports

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,9 @@ class UsersController < ApplicationController
   def show
     if user_signed_in?
       @user = current_user
+      if @user.id == 1
+        Character.where("user_id IS NULL").destroy_all
+      end
     else
       redirect_to new_user_session_path
     end
